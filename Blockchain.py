74bf1cdd5675e48e72b4d2d66e9d5a9ca400683b
snryang <nxwsyang@163.com>
2018-03-27 02:33:59 -0700

    ok

--- Blockchain.py
+++ Blockchain.py
@@ -6,6 +6,7 @@ from time import time
 class Blockchain(object):
     def __init__(self):
         self.chain = []
+        self.nodes = set()
         self.current_transactions = []
         #create the genesis block
         self.new_block(previous_hash=1,proof=100)
@@ -31,12 +32,52 @@ class Blockchain(object):
             'amount':amount,
         })
         return self.last_block['index']+1
+    
     def proof_of_work(self,last_proof):
         proof=0
         while self.valid_proof(last_proof,proof) is False:
             proof+=1
         return proof
     
+    def register_node(self,address):
+        #add a new node to the list of nodes
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self,chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(last_block)
+            print(block)
+            print("\n---------------\n")
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            last_block = block
+            current_index +=1
+        return True
+
+    def resolve_conflicts(self):
+        neighbours = self.nodes
+        new_chain = None
+        #only looking for chains longer than ours
+        max_length = len(self.chain)
+        for node in neighbours:
+            response = requests.get('http://{}/chain'.format(node))
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+        if new_chain:
+            self.chain = new_chain
+            return True
+        return False
+        
     @staticmethod
     def valid_proof(last_proof,proof):
         guess=  "{}{}".format(last_proof, proof).encode()
