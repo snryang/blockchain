7fe2f6e7411c5bd4cb2a3582a6b51001c950dd55
snryang <nxwsyang@163.com>
2018-03-26 23:27:30 -0700

    blockchain ok

--- Blockchain.py
+++ Blockchain.py
@@ -1,20 +1,55 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        #create the genesis block
+        self.new_block(previous_hash=1,proof=100)
 
-    def new_block(self):
+    def new_block(self,proof,previous_hash=None):
         # Creates a new Block and adds it to the chain
-        pass
+        block = {
+            'index':len(self.chain)+1,
+            'timestamp':time(),
+            'transactions':self.current_transactions,
+            'proof':proof,
+            'previous_hash':previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
-    def new_transaction(self):
+    def new_transaction(self,sender,recipient,amount):
         #Adds a new transaction to the list of transactions
-        pass
+        self.current_transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount,
+        })
+        return self.last_block['index']+1
+    def proof_of_work(self,last_proof):
+        proof=0
+        while self.valid_proof(last_proof,proof) is False:
+            proof++
+        return proof
+    
+    @staticmethod
+    def valid_proof(last_proof,proof):
+        guess=f'{last_block}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
         # Hashes a Block
-        pass
+        block_string = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
---
+++ readme.md
@@ -0,0 +1,16 @@
+learn blockchain 
+
+### block data
+` python
+block={
+    'index':1,
+    'timestamp':1506057125.900785
+    'transactions':[{
+        'sender':"852714754545",
+        'recipient':"a77f5cdfa2934df3954",
+        'amount':5,
+    }]
+    'proof':324984774000,
+    'previous_hash':"2cf24dba5fffd54f"
+}
+`
