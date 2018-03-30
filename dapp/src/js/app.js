c89d828860b150894465af724cb54de6cb1cdc60
snryang <nxwsyang@163.com>
2018-03-29 23:38:26 -0700

    add jquery.js and demo code

--- dapp/src/js/app.js
+++ dapp/src/js/app.js
@@ -27,7 +27,12 @@ App = {
     /*
      * Replace me...
      */
-
+    if(typeof web3 != 'undefined'){
+      App.web3Provider = web3.currentProvider;
+    }else{
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
@@ -35,7 +40,12 @@ App = {
     /*
      * Replace me...
      */
-
+    $.getJSON('Adoption.json',function(data){
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      App.contracts.Adoption.setProvider(App.web3Provider);
+      return App.markAdopted();
+    });
     return App.bindEvents();
   },
 
@@ -47,6 +57,19 @@ App = {
     /*
      * Replace me...
      */
+    var adoptionInstance;
+    App.contracts.Adoption.depolyed().then(function(instance){
+      adoptionInstance = instance;
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters){
+      for(i=0;i<adopters.length;i++){
+        if(adopters[i]!=='0x0000000000000000000000000000000000000000'){
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled',true);
+        }
+      }
+    }).catch(function(err){
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -54,6 +77,21 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
+    var adoptionInstance;
+    web3.eth.getAccounts(function(error,accounts){
+      if(error){
+        console.log(error);
+      }
+      var account = accounts[0];
+      App.contracts.Adoption.depolyed().then(function(instance){
+        adoptionInstance = instance;
+        return adoptionInstance.adopt(petId,{from:account});
+      }).then(function(result){
+        return ApplicationCache.markAdopted();
+      }).catch(function(err){
+        console.log(err.message);
+      });
+    });
     /*
      * Replace me...
      */
