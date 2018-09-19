c4479ccc595629412b6e12594a5c66143049bccf
杨文兵 <ywb48431@ly.com>
2018-09-19 16:45:36 +0800

    第三章复读

--- golang/demo/main.go
+++ golang/demo/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "expvar"
-    "fmt"
-    "net/http"
+	"expvar"
+	"fmt"
+	"net/http"
 )
 
 var visits = expvar.NewInt("visits")
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    visits.Add(1)
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	visits.Add(1)
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
-	
+
 }
